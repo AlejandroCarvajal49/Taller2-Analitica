6fd9d77e76c0218f8d9a47ff74471dbf336e4ed2
AlejandroCarvajal49 <a.carvajalt@uniandes.edu.co>
2025-08-12 15:13:19 -0500

    Proporción aurea

--- Fibonnaci.py
+++ Fibonnaci.py
@@ -1,4 +1,3 @@
-#Hazme un código de fibonacci
 def fibonacci(n):
     if n == 0:
         return 0
@@ -7,4 +6,10 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(10))  # Cambia el número para calcular otro término de la serie
+print(fibonacci(10)) 
+
+def proporcion_aurea(n):
+    phi = (1 + 5 ** 0.5) / 2
+    return round(phi ** n / 5 ** 0.5)
+
+print(proporcion_aurea(10))  
